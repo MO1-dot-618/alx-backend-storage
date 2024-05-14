34d7a298153410d569d336d033b0da8165d9c7e3
Malika <malikaoubilla@gmail.com>
2024-05-14 10:39:26 -0700

    100-101

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,29 @@
+"""
+top student
+"""
+
+
+def top_students(mongo_collection):
+    """
+    Returns all students sorted by average score.
+
+    Args:
+        mongo_collection: A PyMongo collection object.
+
+    Returns:
+        A list of dictionaries representing students, sorted by average score.
+        Each dictionary contains the student's information along with the average score.
+    """
+    pipeline = [
+        {"$unwind": "$topics"},
+        {"$group": {
+            "_id": "$_id",
+            "name": {"$first": "$name"},
+            "averageScore": {"$avg": "$topics.score"}
+        }},
+        {"$sort": {"averageScore": -1}}
+    ]
+
+    result = mongo_collection.aggregate(pipeline)
+
+    return list(result)
